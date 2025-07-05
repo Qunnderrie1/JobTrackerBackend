4544f4f1947e4788c34507ad5c0db6e9b5754932
Qunnderrie1 <qunnderrie@gmail.com>
2025-07-04 22:19:43 -0400

    made some changes

--- Backend/Routes/protect.js
+++ Backend/Routes/protect.js
@@ -4,17 +4,20 @@ import { User } from '../Models/UserModel.js'
 
 
 const protect = async (req, res, next) => {
+    //Get the token from the cookie
+    const token = req.cookies.token
 
     console.log('Token from cookie ' + req.cookies.token)
     console.log('Token ' + req.cookies)
     console.log('request ' + req)
 
+    if (!token) {
+        return res.status(401).json({ message: "No Token Provided!" })
+    }
+
+
     try {
-        //Get the token from the cookie
-        const token = req.cookies.token
-        if (!token) {
-            return res.status(401).json({ message: "No Token Provided!" })
-        }
+
         // Verify token
         const decode = jwt.verify(token, process.env.JWT_SECRET)
         // Get user from token
